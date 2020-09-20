c88582a9f5c3c47385a017147bcc5025777bc202
Lily <lilyau0112@gmail.com>
2020-09-20 18:19:46 +0800

    map photos

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,11 +1,19 @@
 import React, {useContext} from "react"
+import Image from '../components/Image'
+import {getClass} from '../utils/index'
 
+import {Context} from '../Context'
 
 function Photos() {
+    const {photos} =useContext(Context)
+    const imgElement = photos.map((img,index) => (
+        <Image key={img.id} img={img} className={getClass(index)}/>
+    ))
 
     return (
         <main className="photos">
-            <h1>Images goes here</h1>
+            {imgElement}
+            
         </main>
     )
 }

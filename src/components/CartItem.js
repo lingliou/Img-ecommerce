858fbe952f7b0d3b422de0ab925f561ff9977470
Lily <lilyau0112@gmail.com>
2020-09-22 01:29:49 +0800

    remove item and calculate price

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {Context} from '../Context'
 
 function CartItem({item}){
+    const{removeFromCart} = useContext(Context)
     return(
         <div className="cart-item">
-            <i className="ri-delete-bin-line"></i>
-            <img src={item.url} width="130px" />
+            <i className="ri-delete-bin-line" onClick={()=>removeFromCart(item)}></i>
+            <img src={item.url} width="130px" height="73px" />
             <p>$5.99</p>
         </div>
     )
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,11 +7,13 @@ function Cart() {
     const cartElement = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
         ))
+    const totalPrice = 5.99 * cartItems.length
+    const Price= totalPrice.toLocaleString("en-US", {style: "currency", currency: "USD"})
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartElement}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total:{Price} </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>

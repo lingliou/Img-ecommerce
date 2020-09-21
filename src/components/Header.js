1f482cda9a04a180c1e7c5441d02a090c0c31b51
Lily <lilyau0112@gmail.com>
2020-09-22 01:18:33 +0800

    cart page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import React from "react"
+import React, {useContext} from "react"
+import {Context} from '../Context'
 import {Link} from "react-router-dom"
 
+
 function Header() {
+
+    const {cartItems} = useContext(Context)
+
+    function cartIcon(){
+        if(cartItems.length !== 0){
+            return <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+        }
+        else{
+            return <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        }
+    }
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart">{cartIcon()}</Link>
         </header>
     )
 }
